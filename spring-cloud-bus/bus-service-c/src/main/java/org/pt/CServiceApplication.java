e79c6d84539f9c65cb9e41df1bd99ed0aa969545
pengtao <3548297839@qq.com>
2025-02-26 20:46:55 +0800

    消息总线 :fire:

--- spring-cloud-bus/bus-service-c/src/main/java/org/pt/CServiceApplication.java
+++ spring-cloud-bus/bus-service-c/src/main/java/org/pt/CServiceApplication.java
@@ -3,11 +3,13 @@ package org.pt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class CServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CServiceApplication.class);
